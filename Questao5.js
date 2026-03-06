7526bbe81e2db7e443d207d525f1672339ffa9b5
Breno Araujo <164903915+BrenolaKKJ@users.noreply.github.com>
2026-03-06 20:41:07 -0300

    Add grade calculation and alert system

---
+++ Questao5.js
@@ -0,0 +1,13 @@
+let nota1 = Number(prompt("Digite a primeira nota:"));
+let nota2 = Number(prompt("Digite a segunda nota:"));
+let nota3 = Number(prompt("Digite a terceira nota:"));
+
+let media = (nota1 + nota2 + nota3) / 3;
+
+if (media >= 7) {
+    alert("Média: " + media + " - Aprovado");
+} else if (media >= 5) {
+    alert("Média: " + media + " - Recuperação");
+} else {
+    alert("Média: " + media + " - Reprovado");
+}
