f97526392f9a2f0bd673811d62f7344ed14bf1b9
Breno Araujo <164903915+BrenolaKKJ@users.noreply.github.com>
2026-03-06 20:45:38 -0300

    Remove exclamation marks from alert messages

--- Questao9.js
+++ Questao9.js
@@ -8,14 +8,14 @@ while (tentativas < 3) {
     senha = prompt("Digite a senha:");
 
     if (usuario === "admin" && senha === "1234") {
-        alert("Login realizado com sucesso!");
+        alert("Login realizado com sucesso");
         break;
     } else {
         tentativas++;
-        alert("Usuário ou senha incorretos!");
+        alert("Usuário ou senha incorretos");
     }
 }
 
 if (tentativas === 3) {
-    alert("Acesso bloqueado!");
+    alert("Acesso bloqueado");
 }
